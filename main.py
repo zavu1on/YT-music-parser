04b6429e10a731576b6a527d41843809a9378059
Mikhail <joinmma@gmail.com>
2022-06-24 13:11:47 +0200

    add multiprocessing

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
 import random
 import slugify
+from multiprocessing import Pool
+from datetime import datetime
 from pytube import YouTube
 from moviepy import editor
 from bs4 import BeautifulSoup
 
-with open('YouTube Music.html', 'r', encoding='utf8') as html:
-    soup = BeautifulSoup(html.read(), 'lxml')
 
-    container = soup.find('ytmusic-playlist-shelf-renderer')
-    tracks = container.find_all('ytmusic-responsive-list-item-renderer')
+def parse_track(data):
+    idx, link = data
 
-    for idx, t in enumerate(tracks):
-        div = t.find('div', class_='title-column')
-        a = div.find('a')
-        link = a.attrs['href'].replace('music.', '').split('&')[0]
+    try:
+        v = YouTube(link)
+        s = v.streams.get_highest_resolution()
+        path = s.download('video')
+    except Exception as e:
+        print(e)
+        print(f'video {idx} raised an exception')
+        return
 
-        print(link)
+    v = editor.VideoFileClip(path)
+    audio = v.audio
 
-        try:
-            v = YouTube(link)
-            s = v.streams.get_highest_resolution()
-            path = s.download('video')
-        except Exception as e:
-            print(e)
-            print(f'video {idx} raised an exception')
-            continue
+    title = path.replace('video', 'music').split('\\')[-1].split('.')[0]
+    title = slugify.slugify(title) or str(random.random())
+    title = f'{idx}_{title}'
 
-        v = editor.VideoFileClip(path)
-        audio = v.audio
+    print(title)
 
-        title = path.replace('video', 'music').split('\\')[-1].split('.')[0]
-        title = slugify.slugify(title) or str(random.random())
-        title = f'{idx}_{title}'
+    audio.write_audiofile(
+        ('\\'.join(path.replace('video', 'music').split('\\')[:-1]) + '\\' + title + '.mp3')
+    )
 
-        print(title)
 
-        audio.write_audiofile(
-            ('\\'.join(path.replace('video', 'music').split('\\')[:-1]) + '\\' + title + '.mp3')
-        )
+if __name__ == '__main__':
+    start_time = datetime.now()
+
+    with open('YouTube Music.html', 'r', encoding='utf8') as html:
+        soup = BeautifulSoup(html.read(), 'lxml')
+
+        container = soup.find('ytmusic-playlist-shelf-renderer')
+        tracks = container.find_all('ytmusic-responsive-list-item-renderer')
+        links = []
+
+        for t in tracks:
+            div = t.find('div', class_='title-column')
+            a = div.find('a')
+            links.append(a.attrs['href'].replace('music.', '').split('&')[0])
+
+        with Pool(10) as pool:
+            pool.map(parse_track, enumerate(links))
+
+    print(f'FINISHED BY {start_time - datetime} SEC')
